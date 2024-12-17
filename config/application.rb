cc7e346239594d608097a133be181666b1adc08a
Ayabanana <a.taba085h@gmail.com>
2024-12-17 14:47:12 +0900

    デプロイ用の変更

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,6 @@ module MyApiApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.assets.enabled = false
+    # config.assets.enabled = false
   end
 end
